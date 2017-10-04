2c2d911935f7677357206b37f8dcacca6a21d5f2
Carlos Flores <cflores@progressly.com>
2017-10-04 17:12:59 -0500

    removed variable from bash script

--- web-installer.sh
+++ web-installer.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-echo "how is your project named?"
-read project
 curl -sS https://codeload.github.com/ZeroDragon/scaffolder/tar.gz/latest | tar -xzv
-mv scaffolder-latest "$project"
+mv scaffolder-latest scaffolder
